c6d50702c7963ea0cdc5b6e8267dda09f189b1a6
AFrenchDude <117642832+AFrenchDude@users.noreply.github.com>
2022-11-22 16:35:47 +0100

    ADD TMP Essentials, Drag'N Drop base files, Gold Display and Spawn Turret Button scripts RETAKE Databases moved to another folder

--- Unity_DreamTD/Assets/Script/Base.cs
+++ Unity_DreamTD/Assets/Script/Base.cs
@@ -7,16 +7,22 @@ using UnityEngine.Events;
 public class Base : Singleton<Base>
 {
     [SerializeField] private int _maxBaseHP = 100;
+    [SerializeField] private int _startingGold = 50;
+    
     [SerializeField]
     private int _currentBaseHP = 1;
+    private int _currentGold = 1;
 
     public int BaseHP => _currentBaseHP;
+    public int Gold => _currentGold;
 
     public UnityEvent BaseDied;
+    public UnityEvent<int> GoldUpdated;
     override protected void Awake()
     {
         base.Awake();
         _currentBaseHP = _maxBaseHP;
+        _currentGold = _startingGold;
     }
 
     public void BaseReceiveDMG(int damage)
@@ -26,5 +32,16 @@ public class Base : Singleton<Base>
         {
             BaseDied.Invoke();
         }
+    }
+
+    public void AddGold(int gold)
+    {
+        _currentGold += gold;
+        GoldUpdated.Invoke(_currentGold);
+    }
+    public void RemoveGold(int gold)
+    {
+        _currentGold -= gold;
+        GoldUpdated.Invoke(_currentGold);
     }
 }
