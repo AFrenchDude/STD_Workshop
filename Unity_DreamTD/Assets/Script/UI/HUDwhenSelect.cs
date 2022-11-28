b7e1be5af9cda3e928d76104ff0647139ac7d0fe
oui <mail.mail@mail.mail>
2022-11-28 13:08:39 +0100

    Add sentry HUD

--- Unity_DreamTD/Assets/Script/UI/HUDwhenSelect.cs
+++ Unity_DreamTD/Assets/Script/UI/HUDwhenSelect.cs
@@ -4,18 +4,29 @@ public class HUDwhenSelect : MonoBehaviour
 {
     public GameObject hudRef;
     
+    //Display HUD
     public void OnClick(bool isActive)
     {
-        hudRef.SetActive(isActive);
+        //If click on train
         if (hudRef.GetComponent<TrainsHUD>() != null)
         {
+            hudRef.SetActive(isActive);
             hudRef.GetComponent<TrainsHUD>().train = transform.parent.gameObject;
             hudRef.GetComponent<TrainsHUD>().PickTrain();
         }
+        //If click on usine
         else if(hudRef.GetComponent<UsineHUD>() != null)
         {
+            hudRef.SetActive(isActive);
             hudRef.GetComponent<UsineHUD>().usineBehaviour = GetComponent<UsineBehaviour>();
             hudRef.GetComponent<UsineHUD>().OnPick();
+        }
+        //If click on tower
+        else if(hudRef.GetComponent<TowerHUD>() != null && gameObject.GetComponent<Tower>().enabled == true)
+        {
+            hudRef.SetActive(isActive);
+            hudRef.GetComponent<TowerHUD>().tower = GetComponentInChildren<TowerGetProjectile>();
+            hudRef.GetComponent<TowerHUD>().OnPick();
         }
     }
 }
