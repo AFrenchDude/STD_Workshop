b7b32532ddfce32e280b77958d287827007c7f75
AFrenchDude <117642832+AFrenchDude@users.noreply.github.com>
2022-11-29 17:37:05 +0100

    ADD every tier1 upgrade for projectiles (Gum, slow, stun, pizza, star), the gum area prefab, the pink material for gum RETAKE every health/damage call to float, ProjectileType to have the opposite of convert type to nightmare method, DamageDone event to send damageable hit, PathFollower to allow slow status, NightmareManager to store initial speed, projectile trap prefab to test features

--- Unity_DreamTD/Assets/Script/DamageSystem/Damager.cs
+++ Unity_DreamTD/Assets/Script/DamageSystem/Damager.cs
@@ -1,31 +1,51 @@
 //By ALBERT Esteban & ALEXANDRE Dorian
 using System.Collections;
 using System.Collections.Generic;
+using System.Diagnostics;
 using UnityEngine;
 using UnityEngine.Events;
 
 public class Damager : MonoBehaviour
 {
-    [SerializeField] private int _attack = 1;
+    [SerializeField] private float _attack = 1;
+    [SerializeField] ProjectileType _attackType;
 
     public void SetDamage(float damage)
     {
         _attack = ((int)damage);
     }
-    public int getDamage
+    public float getDamage
     {
         get { return _attack; }
     }
-
-    public UnityEvent DamageDone;
+    public ProjectileType AttackType => _attackType;
+    public UnityEvent<Damageable> DamageDone;
     private void OnTriggerEnter(Collider other)
     {
         Damageable otherDamageable = other.GetComponent<Damageable>();
 
         if (otherDamageable != null)
         {
-            otherDamageable.TakeDamage(_attack, out int health);
-            DamageDone.Invoke();
+            NightmareData.NighmareType otherNightmareType = otherDamageable.NightmareType;
+            otherDamageable.TakeDamage(_attack, out float health);
+            DamageDone.Invoke(otherDamageable);
         }
+    }
+
+    private float CheckEffectiveness(NightmareData.NighmareType otherNightmareType)
+    {
+        float newdamage = _attack;
+
+        NightmareData.NighmareType projectileNightmareWeak = _attackType.convertProjectileToNightmare();
+        NightmareData.NighmareType projectileNightmareresisted = _attackType.convertProjectileToNightmareResistance();
+        if (projectileNightmareWeak == otherNightmareType)
+        {
+            newdamage = _attack * 1.2f;
+        }
+        if (projectileNightmareresisted == otherNightmareType)
+        {
+            newdamage = _attack * 0.8f;
+        }
+        return newdamage;
     }
 }
