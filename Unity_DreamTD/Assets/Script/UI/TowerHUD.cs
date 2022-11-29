9ddb8496887b9961d9ddff990c25960cab87b3f7
oui <mail.mail@mail.mail>
2022-11-29 18:34:41 +0100

    oui

--- Unity_DreamTD/Assets/Script/UI/TowerHUD.cs
+++ Unity_DreamTD/Assets/Script/UI/TowerHUD.cs
@@ -2,7 +2,6 @@ using System;
 using TMPro;
 using UnityEngine;
 using UnityEngine.UI;
-using static UnityEngine.EventSystems.EventTrigger;
 
 //Made by Melinon Remy
 public class TowerHUD : MonoBehaviour
