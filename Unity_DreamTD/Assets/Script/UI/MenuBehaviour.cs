8d2e71dab3a102c1cad0a0690d91969b26f57448
oui <mail.mail@mail.mail>
2022-12-01 11:23:39 +0100

    Debug option Menu Add pause button add projectile sound when fire

--- Unity_DreamTD/Assets/Script/UI/MenuBehaviour.cs
+++ Unity_DreamTD/Assets/Script/UI/MenuBehaviour.cs
@@ -1,15 +1,9 @@
 using UnityEngine;
-using UnityEngine.SceneManagement;
 
 public class MenuBehaviour : MonoBehaviour
 {
     public void QuitGame()
     {
         Application.Quit();
-    }
-
-    public void SceneLoader(string sceneToLoad)
-    {
-        SceneManager.LoadScene(sceneToLoad);
     }
 }
