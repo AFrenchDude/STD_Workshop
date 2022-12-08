ebb3734beb3e286364b484003504e61b54cf87b9
oui <mail.mail@mail.mail>
2022-12-08 14:38:08 +0100

    Start museum

---
+++ Unity_DreamTD/Assets/Script/UI/MuseumBehaviour.cs
@@ -0,0 +1,20 @@
+using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+
+//Made by Melinon Remy
+public class MuseumBehaviour : MonoBehaviour
+{
+    
+    [SerializeField] TextMeshProUGUI text;
+
+    public void OnMuseumOpen()
+    {
+
+    }
+
+    public void ChangeEnemyDescription()
+    {
+
+    }
+}
