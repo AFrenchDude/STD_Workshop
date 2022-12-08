6b1e45e57e4ce751f2c1ea83d46372acb1b07d8a
ADorian <dorian.alexandre38@gmail.com>
2022-12-08 01:20:38 +0100

    UPDATE Player (New Basement)

--- Unity_DreamTD/Assets/Script/Utilities/BaseHPManager.cs
+++ Unity_DreamTD/Assets/Script/Utilities/BaseHPManager.cs
@@ -6,24 +6,12 @@ using UnityEngine.UI;
 public class BaseHPManager : MonoBehaviour
 {
     [SerializeField] private TextMeshProUGUI lifeText;
-    [SerializeField] private Slider lifeBar;
-    [SerializeField] private Image lifeBarFill;
+    //[SerializeField] private Slider lifeBar;
+    //[SerializeField] private Image lifeBarFill;
 
     public void OnLifeChange(int newLife)
     {
-        lifeText.SetText("Life : " + newLife);
-        lifeBar.value = (float)newLife / 100;
-        if(newLife > 50)
-        {
-            lifeBarFill.color = Color.green;
-        }
-        if(newLife <= 50 && newLife > 10)
-        {
-            lifeBarFill.color = Color.yellow;
-        }
-        else if(newLife <= 10)
-        {
-            lifeBarFill.color = Color.red;
-        }
+        lifeText.SetText(newLife.ToString());
+
     }
 }
