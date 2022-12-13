3dcfb72b24b4d88f043a1666e61d6c0888bc7902
DaFrenchSpeaker <e.albert@game-sup.fr>
2022-12-13 03:13:50 +0100

    RETAKE ScoreText to change icon color instead of enabling it, DissolveVFX scripts to prevent launching the function once it's played, player prefab to add endgame screens visuals, level1save to reset score

--- Unity_DreamTD/Assets/Script/Utilities/DissolveVFXApplierObjects.cs
+++ Unity_DreamTD/Assets/Script/Utilities/DissolveVFXApplierObjects.cs
@@ -33,6 +33,10 @@ public class DissolveVFXApplierObjects : MonoBehaviour
     [ContextMenu("Dissolve")]
     public void Dissolve()
     {
+        if (_waitingToDissolve)
+        {
+            return;
+        }
         SpawnVFXs();
         SetDissolveMaterial();
         _waitingToDissolve = true;
